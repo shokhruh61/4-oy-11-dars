e262a7539ec996444ca993cba4a593a651a32558
shokhruh61 <shohruxmahmudov61@gmail.com>
2024-10-14 15:55:16 +0500

    js

--- js/main.js
+++ js/main.js
@@ -51,17 +51,49 @@
 
 // console.log("Umumiy yig'indi: " + result);
 
-// 6-masala
+// // 6-masala
 
-function isPositive(arg) {
-  if (arg > 0) {
-    return "musbat";
-  } else {
-    return "musbat emas";
-  }
-}
+// function isPositive(arg) {
+//   if (arg > 0) {
+//     return "musbat";
+//   } else {
+//     return "musbat emas";
+//   }
+// }
 
-// Funksiyani chaqirib tekshirish
-console.log(isPositive(5));
-console.log(isPositive(-3));
-console.log(isPositive(0));
+// // Funksiyani chaqirib tekshirish
+// console.log(isPositive(5));
+// console.log(isPositive(-3));
+// console.log(isPositive(0));
+
+// // 7-masala
+
+// function wordLength(word) {
+//   return word.length;
+// }
+
+// console.log(wordLength("Hello world!"));
+
+// // 8-masala
+
+// function concatenateStrings(string1, string2) {
+//   return string1 + string2;
+// }
+
+// console.log(concatenateStrings("Hello,", " World!"));
+
+// // 9-masala
+
+// function findSmallestElement(arr) {
+//   return Math.min(...arr);
+// }
+
+// console.log(findSmallestElement([5, 2, 9, 1, 7])); 
+
+// // 10-masala
+
+// function reverseString(str) {
+//    return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString("hello"));
